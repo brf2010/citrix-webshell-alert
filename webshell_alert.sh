688ea7e56ed3bed3efd8c45bda6aa7db90b2f24a
Bryan Fisher <brf2010@med.cornell.edu>
2025-08-26 14:49:07 -0400

    1st working version

--- webshell_alert.sh
+++ webshell_alert.sh
@@ -6,6 +6,8 @@ FILE_PATH="$1"
 SPLUNK_URL="$2"
 HEC_TOKEN="$3"
 
+HOSTNAME=$(hostname)
+
 if [ -z "$FILE_PATH" ] || [ -z "$SPLUNK_URL" ] || [ -z "$HEC_TOKEN" ]; then
   echo "Usage: $0 <file_path> <splunk_url> <hec_token>"
   exit 1
@@ -20,13 +22,10 @@ fi
 FILE_CONTENT=$(cat "$FILE_PATH")
 
 # Escape double quotes and backslashes for JSON
-ESCAPED_CONTENT=$(printf '%s' "$FILE_CONTENT" | sed 's/\\/\\\\/g; s/"/\\"/g')
-
-# Construct JSON payload manually
-JSON_PAYLOAD="{\"event\": \"$ESCAPED_CONTENT\"}"
+ESCAPED_CONTENT=$(sed -e 's/\\/\\\\/g' -e 's/"/\\"/g' -e ':a;N;$!ba;s/\n/\\n/g' "$FILE_PATH")
 
 # Send to Splunk HEC
 curl -k "$SPLUNK_URL" \
   -H "Authorization: Splunk $HEC_TOKEN" \
-  -H "Content-Type: application/json" \
-  -d "$JSON_PAYLOAD"
+  -d "{\"host\": \"$HOSTNAME\", \
+  \"event\": \"$ESCAPED_CONTENT\"}"
