1fac4da4e9689603ffceb3fb921c05f9b5e37863
Bryan Fisher <brf2010@med.cornell.edu>
2025-08-27 10:55:28 -0400

    Fix comments, pointless variable rename

--- webshell_alert.sh
+++ webshell_alert.sh
@@ -7,35 +7,28 @@ HEC_TOKEN="$2"
 CHECK_SCRIPT_PATH="/var/nsinstall/TLPCLEAR_check_script_cve-2025-6543-v1.8.sh"
 HOSTNAME=$(hostname)
 
-# run the check script
-
 # Run the check script and capture its output
 output=$(sh $CHECK_SCRIPT_PATH)
 
 # Extract the log path using grep and sed
 log_path=$(echo "$output" | grep "Log saved to" | sed 's/^.*Log saved to //')
-
-# Use the log path later in the script
 echo "Captured log path: $log_path"
-
-FILE_PATH=$log_path
-
 
 if [ -z "$SPLUNK_URL" ] || [ -z "$HEC_TOKEN" ]; then
   echo "Usage: $0 <splunk_url> <hec_token>"
   exit 1
 fi
 
-if [ ! -f "$FILE_PATH" ]; then
-  echo "Error: File '$FILE_PATH' not found."
+if [ ! -f "$log_path" ]; then
+  echo "Error: File '$log_path' not found."
   exit 1
 fi
 
 # Read file content
-FILE_CONTENT=$(cat "$FILE_PATH")
+FILE_CONTENT=$(cat "$log_path")
 
 # Escape double quotes and backslashes for JSON
-ESCAPED_CONTENT=$(sed -e 's/\\/\\\\/g' -e 's/"/\\"/g' "$FILE_PATH" | perl -0777 -pe 's/\n/\\n/g')
+ESCAPED_CONTENT=$(sed -e 's/\\/\\\\/g' -e 's/"/\\"/g' "$log_path" | perl -0777 -pe 's/\n/\\n/g')
 
 # Send to Splunk HEC
 curl -k "$SPLUNK_URL" \
