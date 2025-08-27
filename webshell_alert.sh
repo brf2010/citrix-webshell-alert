4512618c73d8cb918ca6d180282e08661eb545c9
Bryan Fisher <brf2010@med.cornell.edu>
2025-08-27 10:56:33 -0400

    move usage check to top

--- webshell_alert.sh
+++ webshell_alert.sh
@@ -2,6 +2,11 @@
 # citrix compromise alerting wrapper script, written by Bryan Fisher <brf2010@med.cornell.edu> <bryan.fisher797@gmail.com>
 # Usage: ./webshell_alert.sh <splunk_url> <hec_token>
 
+if [ -z "$SPLUNK_URL" ] || [ -z "$HEC_TOKEN" ]; then
+  echo "Usage: $0 <splunk_url> <hec_token>"
+  exit 1
+fi
+
 SPLUNK_URL="$1"
 HEC_TOKEN="$2"
 CHECK_SCRIPT_PATH="/var/nsinstall/TLPCLEAR_check_script_cve-2025-6543-v1.8.sh"
@@ -13,11 +18,6 @@ output=$(sh $CHECK_SCRIPT_PATH)
 # Extract the log path using grep and sed
 log_path=$(echo "$output" | grep "Log saved to" | sed 's/^.*Log saved to //')
 echo "Captured log path: $log_path"
-
-if [ -z "$SPLUNK_URL" ] || [ -z "$HEC_TOKEN" ]; then
-  echo "Usage: $0 <splunk_url> <hec_token>"
-  exit 1
-fi
 
 if [ ! -f "$log_path" ]; then
   echo "Error: File '$log_path' not found."
