aab94880d0ee41a1bee51c878861a0ffb29b8616
Bryan Fisher <brf2010@med.cornell.edu>
2025-08-28 14:31:21 -0400

    test deploy script

---
+++ deploy.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/expect
+# automatic deployment script for citrix ADC systems. 
+# there's probably a better way to do this with ansible but i'm not smart enough to figure it out
+
+set user [lrange $argv 0 0]
+set remote_server [lrange $argv 1 1]
+
+stty -echo
+send_user -- "\nPassword for $user@$remote_server: "
+expect_user -re "(.*)\n"
+send_user "\n"
+stty echo
+set pass $expect_out(1,string)
+
+set timeout 10
+spawn ssh -M $user@$remote_server
+
+# set fh [open $infile r]
+
+while 1 {
+  expect {
+    # "no)?"      {send "yes\r"}
+    "denied" {
+#                log_file expect_msg.log
+                send_log "Can't login to $remote_server. Check username and password\n";
+                exit 1
+             }
+    "failed" {
+#                log_file expect_msg.log
+                send_log "Host $remote_server exists. Check ssh_hosts file\n";
+                exit 3
+             }
+    timeout {
+#                log_file expect_msg.log
+                send_log "Timeout problem. Host $remote_server doesn't respond\n";
+                exit 4
+            }
+    "refused" {
+#                log_file expect_msg.log
+                send_log "Host $remote_server refused to SSH.\n"
+#                log_file
+              }
+    "assword:" {
+                send $pass\n
+                }
+    ">"         {
+                send -- "shell\n"
+                break
+                }
+    #"#"        {break}
+  }
+}
+
+set send_slow {10 .005}
+
+expect "#"
+
+# update root crontab
+send "cat > crontab\n"
+
+while {[gets crontab read_line] != -1} {
+ 	send -s "$read_line\r"
+ }
+
+send "\x04"
+expect "#"
+send "echo success\n"
+expect "#"
+send "exit\n"
+
+# close $fh
